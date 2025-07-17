3a1c979538545065f53ddbe97b40923fd27edbc2
DeyvidF <143139369+DeyvidF@users.noreply.github.com>
2025-07-17 10:55:27 -0300

    starting the project with html and beginning css

---
+++ script.js
@@ -0,0 +1,6 @@
+let number = document.getElementById("number")
+let decrease = document.getElementById("decrease")
+let reset = document.getElementById("reset")
+let increase = documento.getElementById("increase")
+
+
